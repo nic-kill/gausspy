282fcb087714ecc0cec8190fa85f95410b65e38b
nickill <nicholas.killerby-smith@anu.edu.au>
2021-02-22 15:45:02 +1100

    commented out the elif to test how it should have behaved and correct from that point first

--- gausspy/AGD_decomposer.py
+++ gausspy/AGD_decomposer.py
@@ -40,10 +40,10 @@ def paramvec_to_lmfit(paramvec):
     for i in range(len(paramvec)):
         if i < ncomps: #for the amplitudes in opacity 
             params.add("a" + str(i + 1), value=paramvec[i], min=0.0005897952*3)
-        elif i >= ncomps and i < 2 * ncomps: #for the widths in opacity
-            params.add("w" + str(i + 1), value=paramvec[i], 
-            min=np.max([np.sqrt(0.055*3/(21.866*(1-np.exp(-3*0.0005897952))))])
-        )
+        #elif i >= ncomps and i < 2 * ncomps: #for the widths in opacity
+        #    params.add("w" + str(i + 1), value=paramvec[i], 
+        #    min=np.max([np.sqrt(0.055*3/(21.866*(1-np.exp(-3*0.0005897952))))])
+        #)
         else: #for else whcih currently is just the position
             params.add("p" + str(i + 1), value=paramvec[i])
     print(f'printing {len(params)} abs comps')
