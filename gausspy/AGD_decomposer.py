8e5e9e1f2b95240f8447f876ad8f02f13283809a
nickill <nicholas.killerby-smith@anu.edu.au>
2021-03-11 19:06:29 +1100

    delted else cond and assignment that would never be used

--- gausspy/AGD_decomposer.py
+++ gausspy/AGD_decomposer.py
@@ -1185,8 +1185,7 @@ def AGD_double(
     ncomps_g3 = agd3["N_components"]
     if ncomps_g3 > 0:
         params_g3 = np.concatenate([agd3["amps"], agd3["FWHMs"], agd3["means"]])
-    else:
-        params_g3 = []
+
 
     # Check for phase three components, make final guess list
     # ------------------------------------------------------
